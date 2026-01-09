68dd79d3ab6b1f0d19ad87a1addc533014d503f1
LeonidZharenko <leonidzarenko13@gmail.com>
2026-01-09 17:07:58 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,19 @@
+return {
+    Aimbot = {
+        fovRadius = 150,
+        showFovCircle = false,
+        targetPart = "Head",
+        smoothness = 0.12,
+        wallCheck = true,
+        fullTarget = false,
+        holdPkmMode = false,
+    },
+    ESP = {
+        BoxEnabled = true,
+        TracerEnabled = true,
+        NameEnabled = true,
+        ShowDistance = true,
+        TeamCheck = true,
+        MaxRenderDistance = 5000,
+    },
+}

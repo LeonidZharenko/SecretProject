aa410f352d1bfc45e55ddd917edb8969a4c58aca
LeonidZharenko <leonidzarenko13@gmail.com>
2026-01-13 13:58:24 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,19 +1,39 @@
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+-- Orion Lib вместо Fluent (работает, где Fluent блочат)
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
 
-local Window = Fluent:CreateWindow({
-    Title = "My Exploit | Aimbot + ESP"
+local Window = OrionLib:MakeWindow({
+    Name = "My Exploit | Aimbot + ESP",
+    HidePremium = false,
+    SaveConfig = true,
+    ConfigFolder = "MyExploit"
 })
 
-local Config = loadstring(game:HttpGet("https://github.com/LeonidZharenko/SecretProject/blob/main/config.lua"))()
+-- Tabs
+local AimbotTab = Window:MakeTab({
+    Name = "Aimbot",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
 
-local Aimbot = loadstring(game:HttpGet("https://github.com/LeonidZharenko/SecretProject/blob/main/modules/Aimbot.lua"))()
-Aimbot.Init(Config, Window:AddTab({Title = "Aimbot"}))
+local VisualTab = Window:MakeTab({
+    Name = "Visual",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
 
-local ESP = loadstring(game:HttpGet("https://github.com/LeonidZharenko/SecretProject/blob/main/modules/ESP.lua"))()
-ESP.Init(Config, Window:AddTab({Title = "Visual"}))
+-- Загрузка конфига
+local Config = loadstring(game:HttpGet("https://raw.githubusercontent.com/LeonidZharenko/SecretProject/main/config.lua"))()
 
-Fluent:Notify({
-    Title = "Success",
-    Content = "Exploit загружен!",
-    Duration = 5
+-- Aimbot модуль
+local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/LeonidZharenko/SecretProject/main/modules/Aimbot.lua"))()
+Aimbot.Init(Config, AimbotTab)  -- передаём вкладку Orion
+
+-- ESP модуль
+local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/LeonidZharenko/SecretProject/main/modules/ESP.lua"))()
+ESP.Init(Config, VisualTab)  -- передаём вкладку Orion
+
+OrionLib:MakeNotification({
+    Name = "Success",
+    Content = "Exploit загружен! Нажми RightControl для скрытия окна",
+    Time = 5
 })

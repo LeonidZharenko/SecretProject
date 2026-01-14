fa4c00b8f95074bd0e732abae2d100a8b30cfd56
LeonidZharenko <leonidzarenko13@gmail.com>
2026-01-14 21:33:16 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,8 @@
--- main.lua - Супер минимальная версия
 local Players = game:GetService("Players")
 while not Players.LocalPlayer do wait() end
 
 -- 1 строка для ESP
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ваш_юзернейм/mm2-esp/main/modules/esp.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LeonidZharenko/SecretProject/main/modules/ESP.lua"))()
 
 -- 2 строка для Aimbot (опционально)
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/ваш_юзернейм/mm2-esp/main/modules/aimbot.lua"))()
